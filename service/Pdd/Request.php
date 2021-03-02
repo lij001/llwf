6f48688faf15a4b3e71f81e8d0f801952460ca58
liju <q123123>
2021-03-02 23:53:24 +0800

    fff

--- service/Pdd/Request.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace app\api\service\Pdd;
-
-abstract class Request
-{
-    const TYPE_DDK_GOODS_DETAIL = 'pdd.ddk.goods.detail';   // 多多进宝商品详情查询
-    const TYPE_DDK_GOODS_SEARCH = 'pdd.ddk.goods.search';   // 多多进宝商品查询
-}
